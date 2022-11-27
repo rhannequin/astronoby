9cacec4c5d66cc6d2dd6e96c84c09648792c0646
Rémy Hannequin <remy.hannequin@gmail.com>
2022-11-28 00:11:50 +0100

    Use angle object in equatorial's hour angle calculation

--- lib/astronoby/coordinates/equatorial.rb
+++ lib/astronoby/coordinates/equatorial.rb
@@ -14,26 +14,25 @@ module Astronoby
       end
 
       def to_horizontal(time:, latitude:, longitude:)
-        hour_angle = Astronoby::Util::Time.local_sidereal_time(
+        hour_angle_value = Astronoby::Util::Time.local_sidereal_time(
           time: time,
           longitude: longitude
         ) - right_ascension.to_degrees.value
 
-        hour_angle += 24 if hour_angle.negative?
-        hour_angle -= 24 if hour_angle > 24
-        hour_angle_degree = hour_angle * BigDecimal("15")
-        hour_angle_radians = Astronoby::Util::Trigonometry.to_radians(hour_angle_degree)
+        hour_angle_value += 24 if hour_angle_value.negative?
+        hour_angle_value -= 24 if hour_angle_value > 24
+        hour_angle = Astronoby::Angle.as_degrees(hour_angle_value * BigDecimal("15"))
 
         latitude_radians = Astronoby::Util::Trigonometry.to_radians(latitude)
 
         t0 = Math.sin(@declination.to_radians.value) * Math.sin(latitude_radians) +
-          Math.cos(@declination.to_radians.value) * Math.cos(latitude_radians) * Math.cos(hour_angle_radians)
+          Math.cos(@declination.to_radians.value) * Math.cos(latitude_radians) * Math.cos(hour_angle.to_radians.value)
         altitude = Astronoby::Angle.as_radians(Math.asin(t0))
 
         t1 = Math.sin(@declination.to_radians.value) -
           Math.sin(latitude_radians) * Math.sin(altitude.to_radians.value)
         t2 = t1 / (Math.cos(latitude_radians) * Math.cos(altitude.to_radians.value))
-        sin_hour_angle = Math.sin(hour_angle_radians)
+        sin_hour_angle = Math.sin(hour_angle.to_radians.value)
         azimuth = Astronoby::Angle.as_radians(Math.acos(t2))
         if sin_hour_angle.positive?
           azimuth = Astronoby::Angle.as_degrees(BigDecimal("360") - azimuth.to_degrees.value)
