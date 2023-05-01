15483c4e8b54fad34bdaac96420bf0ee07428c7d
Rémy Hannequin <remy@thoughtbot.com>
2023-05-01 16:57:01 +0200

    Compute eccentric anomaly from Kepler's equation
    This introduces an iterative method to compute the eccentric anomaly
    from the mean anomaly and orbital eccentricity, based on the
    Newton/Raphson method.
    
    This method expects a precision parameter and a iterations count limit.

---
+++ lib/astronoby/util/astrodynamics.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+module Astronoby
+  module Util
+    module Astrodynamics
+      class << self
+        # Source:
+        #  Title: Celestial Calculations
+        #  Author: J. L. Lawrence
+        #  Edition: MIT Press
+        #  Chapter: 4 - Orbits and Coordinate Systems
+        def eccentric_anomaly_newton_raphson(
+          mean_anomaly,
+          orbital_eccentricity,
+          precision,
+          maximum_iteration_count,
+          current_iteration = 0,
+          solution_on_previous_interation = nil
+        )
+          previous_solution = solution_on_previous_interation&.to_radians&.value
+
+          solution = if current_iteration == 0
+            if orbital_eccentricity <= 0.75
+              mean_anomaly.to_radians.value
+            else
+              Math::PI
+            end
+          else
+            previous_solution - (
+              (
+                previous_solution -
+                orbital_eccentricity * Math.sin(previous_solution) -
+                mean_anomaly.to_radians.value
+              ) / (
+                1 - orbital_eccentricity * Math.cos(previous_solution)
+              )
+            )
+          end
+
+          if current_iteration >= maximum_iteration_count ||
+              (
+                solution_on_previous_interation &&
+                (solution - previous_solution).abs <= precision
+              )
+            return Astronoby::Angle.as_radians(solution)
+          end
+
+          eccentric_anomaly_newton_raphson(
+            mean_anomaly,
+            orbital_eccentricity,
+            precision,
+            maximum_iteration_count,
+            current_iteration + 1,
+            Astronoby::Angle.as_radians(solution)
+          )
+        end
+      end
+    end
+  end
+end
---
+++ spec/astronoby/util/astrodynamics_spec.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+RSpec.describe Astronoby::Util::Astrodynamics do
+  describe "::eccentric_anomaly_newton_raphson" do
+    it "returns an angle" do
+      mean_anomaly = Astronoby::Angle.as_radians(0.431845)
+      orbital_eccentricity = 0.5
+      precision = 2e-06
+      iterations = 10
+
+      solution = described_class.eccentric_anomaly_newton_raphson(
+        mean_anomaly,
+        orbital_eccentricity,
+        precision,
+        iterations
+      )
+
+      expect(solution).to be_a(Astronoby::Angle)
+    end
+
+    # Source:
+    #  Title: Celestial Calculations
+    #  Author: J. L. Lawrence
+    #  Edition: MIT Press
+    #  Chapter: 4 - Orbits and Coordinate Systems
+    it "computes and returns an approximate solution" do
+      mean_anomaly = Astronoby::Angle.as_radians(0.431845)
+      orbital_eccentricity = 0.5
+      precision = 2e-06
+      iterations = 10
+
+      solution = described_class.eccentric_anomaly_newton_raphson(
+        mean_anomaly,
+        orbital_eccentricity,
+        precision,
+        iterations
+      )
+
+      expect(solution.value).to eq(0.78539851485077)
+    end
+
+    # Source:
+    #  Title: Celestial Calculations
+    #  Author: J. L. Lawrence
+    #  Edition: MIT Press
+    #  Chapter: 4 - Orbits and Coordinate Systems
+    it "computes and returns an approximate solution" do
+      mean_anomaly = Astronoby::Angle.as_degrees(5.498078)
+      orbital_eccentricity = 0.00035
+      precision = 2e-06
+      iterations = 10
+
+      solution = described_class.eccentric_anomaly_newton_raphson(
+        mean_anomaly,
+        orbital_eccentricity,
+        precision,
+        iterations
+      )
+
+      expect(solution.to_degrees.value).to be_within(precision).of(5.5)
+    end
+
+    # Source:
+    #  Title: Celestial Calculations
+    #  Author: J. L. Lawrence
+    #  Edition: MIT Press
+    #  Chapter: 4 - Orbits and Coordinate Systems
+    it "computes and returns an approximate solution" do
+      mean_anomaly = Astronoby::Angle.as_degrees(5.498078)
+      orbital_eccentricity = 0.6813025
+      precision = 2e-06
+      iterations = 10
+
+      solution = described_class.eccentric_anomaly_newton_raphson(
+        mean_anomaly,
+        orbital_eccentricity,
+        precision,
+        iterations
+      )
+
+      expect(solution.to_degrees.value).to be_within(precision).of(16.744355)
+    end
+
+    # Source:
+    #  Title: Celestial Calculations
+    #  Author: J. L. Lawrence
+    #  Edition: MIT Press
+    #  Chapter: 4 - Orbits and Coordinate Systems
+    it "computes and returns an approximate solution" do
+      mean_anomaly = Astronoby::Angle.as_degrees(5.498078)
+      orbital_eccentricity = 0.85
+      precision = 2e-06
+      iterations = 10
+
+      solution = described_class.eccentric_anomaly_newton_raphson(
+        mean_anomaly,
+        orbital_eccentricity,
+        precision,
+        iterations
+      )
+
+      expect(solution.to_degrees.value).to be_within(precision).of(29.422286)
+    end
+  end
+end
