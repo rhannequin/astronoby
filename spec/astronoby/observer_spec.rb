c2869ad2c66f5067f80c4f1adb5a142749066c61
Rémy Hannequin <remy.hannequin@gmail.com>
2024-04-11 11:12:37 +0200

    Enable equivalence and hash equality to Observer (#57)
    An observer can be seen as a value object. If all the characteristics of
    an observer are equal to those of another, they can be considered as the
    same.
    
    This change implements `#==` (and its alias `#eql?`) and `#hash` to
    implement value object behaviours.

--- spec/astronoby/observer_spec.rb
+++ spec/astronoby/observer_spec.rb
@@ -1,6 +1,90 @@
 # frozen_string_literal: true
 
+require "set" # rubocop:disable Lint/RedundantRequireStatement
+
 RSpec.describe Astronoby::Observer do
+  describe "equivalence (#==)" do
+    it "returns true when the observers are equivalent" do
+      observer1 = described_class.new(
+        latitude: Astronoby::Angle.from_degrees(45),
+        longitude: Astronoby::Angle.from_degrees(90),
+        elevation: 100,
+        temperature: 280,
+        pressure: 10
+      )
+      observer2 = described_class.new(
+        latitude: Astronoby::Angle.from_degrees(45),
+        longitude: Astronoby::Angle.from_degrees(90),
+        elevation: 100,
+        temperature: 280,
+        pressure: 10
+      )
+
+      expect(observer1).to eq observer2
+    end
+
+    it "returns true when the observers are not equivalent" do
+      observer1 = described_class.new(
+        latitude: Astronoby::Angle.from_degrees(45),
+        longitude: Astronoby::Angle.from_degrees(90),
+        elevation: 100,
+        temperature: 280,
+        pressure: 10
+      )
+      observer2 = described_class.new(
+        latitude: Astronoby::Angle.from_degrees(45),
+        longitude: Astronoby::Angle.from_degrees(90),
+        elevation: 100,
+        temperature: 280,
+        pressure: 15
+      )
+
+      expect(observer1).not_to eq observer2
+    end
+  end
+
+  describe "hash equality" do
+    it "makes an angle foundable as a Hash key" do
+      observer1 = described_class.new(
+        latitude: Astronoby::Angle.from_degrees(45),
+        longitude: Astronoby::Angle.from_degrees(90),
+        elevation: 100,
+        temperature: 280,
+        pressure: 10
+      )
+      observer2 = described_class.new(
+        latitude: Astronoby::Angle.from_degrees(45),
+        longitude: Astronoby::Angle.from_degrees(90),
+        elevation: 100,
+        temperature: 280,
+        pressure: 10
+      )
+      map = {observer1 => :observer}
+
+      expect(map[observer2]).to eq :observer
+    end
+
+    it "makes an angle foundable in a Set" do
+      observer1 = described_class.new(
+        latitude: Astronoby::Angle.from_degrees(45),
+        longitude: Astronoby::Angle.from_degrees(90),
+        elevation: 100,
+        temperature: 280,
+        pressure: 10
+      )
+      observer2 = described_class.new(
+        latitude: Astronoby::Angle.from_degrees(45),
+        longitude: Astronoby::Angle.from_degrees(90),
+        elevation: 100,
+        temperature: 280,
+        pressure: 10
+      )
+      set = Set.new([observer1])
+
+      expect(set.include?(observer2)).to be true
+    end
+  end
+
   describe "#pressure" do
     it "returns the computed pression in millibars" do
       latitude = Astronoby::Angle.from_degrees(0)
