7e534ec5793c78f96f303e45f84a1071e67c6ba2
Rémy Hannequin <remy@thoughtbot.com>
2025-02-24 11:13:41 +0100

    Precession matrix for 2006 P03 model
    This enables to generate a rotation matrix to apply the 2006 P03 astronomical model for precession.
    
    Also called "precession of equinoxes", precession is a change of the orientation of the Earth's rotational axis. It is a large but slow shift of the rotation axis which changes the direction of the celestial poles.

--- spec/astronoby/precession_spec.rb
+++ spec/astronoby/precession_spec.rb
@@ -107,4 +107,44 @@ RSpec.describe Astronoby::Precession do
       )
     end
   end
+
+  describe "#matrix" do
+    it "returns the right matrix for 2025-08-01" do
+      time = Time.utc(2025, 8, 1)
+      instant = Astronoby::Instant.from_time(time)
+
+      matrix = described_class.new(instant).matrix
+
+      expect(matrix).to eq(
+        Matrix[
+          [0.9999805493402671, -0.00572043918489605, -0.002485461057708681],
+          [0.0057204392635234564, 0.999983638128416, -7.077398710142457e-06],
+          [0.002485460876742937, -7.140667972360681e-06, 0.9999969112118503]]
+      )
+      # Skyfield: [
+      #   [ 0.999980549   -0.00572043919 -0.00248546106]
+      #   [ 0.00572043926  0.999983638   -7.07739871e-06]
+      #   [ 0.00248546088 -7.14066798e-06 0.999996911]
+      # ]
+    end
+
+    it "returns the right matrix for 2050-01-01" do
+      time = Time.utc(2050, 1, 1)
+      instant = Astronoby::Instant.from_time(time)
+
+      matrix = described_class.new(instant).matrix
+
+      expect(matrix).to eq(
+        Matrix[
+          [0.9999256847034389, -0.011181602311713876, -0.004857657882310543],
+          [0.011181602603764246, 0.9999374835602728, -2.7099087538506872e-05],
+          [0.004857657210054194, -2.7219326363292495e-05, 0.9999882011431624]]
+      )
+      # Skyfield: [
+      #   [ 0.999925685   -0.0111816023   -0.00485765788]
+      #   [ 0.0111816026   0.999937484    -2.70990875e-05]
+      #   [ 0.00485765721 -2.72193264e-05  0.999988201]
+      # ]
+    end
+  end
 end
