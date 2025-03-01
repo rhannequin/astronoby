92ecfece1e8a9b455972dff4cb5f21d97d74002b
Rémy Hannequin <remy.hannequin@gmail.com>
2025-03-01 07:48:44 +0100

    New nutation model (#141)
    This introduces `Astronoby::Nutation2`, temporarily named this way, as the new nutation model for Astronoby.
    
    The astronomical nutation is a phenomenon which causes the orientation of the axis of rotation of a spinning astronomical object to vary over time.
    
    It contributes to about 2% of the total precession, the long-period rotation of the Earth's rotation axis.
    
    `Astronoby::Nutation2` relies on official terms from the IAU 2000B model, which is a simplified but still extremely accurate version of the IAU 2000A model.
    
    IAU2000B contains the 77 most significant terms from the 1365 terms from IAU2000A, while it only sacrifices 1 milliarcsecond of accuracy throughout 1995-2025.
    
    The matrix product of the precession matrix and the nutation matrix accounts for the total precession effect. This is necessary to compute the apparent position of a celestial object as seen from an observer on Earth.

---
+++ spec/astronoby/nutation2_spec.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+RSpec.describe Astronoby::Nutation2 do
+  describe "::matrix_for" do
+    it "returns a matrix of 3x3" do
+      time = Time.utc(2025, 8, 1)
+      instant = Astronoby::Instant.from_time(time)
+
+      matrix = described_class.matrix_for(instant)
+
+      expect(matrix.row_size).to eq(3)
+      expect(matrix.column_size).to eq(3)
+    end
+  end
+
+  describe "#nutation_in_longitude" do
+    it "returns the right value for 2025-08-01" do
+      time = Time.utc(2025, 8, 1)
+      instant = Astronoby::Instant.from_time(time)
+      nutation = described_class.new(instant: instant)
+
+      nutation_in_longitude = nutation.nutation_in_longitude
+
+      expect(nutation_in_longitude.str(:dms))
+        .to eq("+0° 0′ 3.8211″")
+      # Skyfield: +0° 0′ 3.8213″
+    end
+
+    it "returns the right value for 2050-01-01" do
+      time = Time.utc(2050, 1, 1)
+      instant = Astronoby::Instant.from_time(time)
+      nutation = described_class.new(instant: instant)
+
+      nutation_in_longitude = nutation.nutation_in_longitude
+
+      expect(nutation_in_longitude.str(:dms))
+        .to eq("+0° 0′ 15.1713″")
+      # Skyfield: +0° 0′ 15.1714″
+    end
+  end
+
+  describe "#nutation_in_obliquity" do
+    it "returns the right value for 2025-08-01" do
+      time = Time.utc(2025, 8, 1)
+      instant = Astronoby::Instant.from_time(time)
+      nutation = described_class.new(instant: instant)
+
+      nutation_in_obliquity = nutation.nutation_in_obliquity
+
+      expect(nutation_in_obliquity.str(:dms))
+        .to eq("+0° 0′ 8.9108″")
+      # Skyfield: +0° 0′ 8.9108″
+    end
+
+    it "returns the right value for 2050-01-01" do
+      time = Time.utc(2050, 1, 1)
+      instant = Astronoby::Instant.from_time(time)
+      nutation = described_class.new(instant: instant)
+
+      nutation_in_obliquity = nutation.nutation_in_obliquity
+
+      expect(nutation_in_obliquity.str(:dms))
+        .to eq("-0° 0′ 5.3301″")
+      # Skyfield: -0° 0′ 5.3297″
+    end
+  end
+end
