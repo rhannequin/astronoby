2664f302eafdd89b3cc71b4329ed774bce117c87
Rémy Hannequin <remy@thoughtbot.com>
2025-05-19 22:43:13 +0200

    Cache positions in RTS calculator

--- lib/astronoby/events/rise_transit_set_calculator.rb
+++ lib/astronoby/events/rise_transit_set_calculator.rb
@@ -268,8 +268,9 @@ module Astronoby
     end
 
     def calculate_positions_at_instants(instants)
+      @positions_cache ||= {}
       instants.map do |instant|
-        @body
+        @positions_cache[instant] ||= @body
           .new(instant: instant, ephem: @ephem)
           .observed_by(@observer)
       end
@@ -347,6 +348,7 @@ module Astronoby
       @sample_instants = nil
       @start_instant = nil
       @end_instant = nil
+      @positions_cache = nil
     end
   end
 end
