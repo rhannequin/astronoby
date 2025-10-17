2b703d1aed9d34abae7a282a6412b2d3a53dd8ec
Rémy Hannequin <remy@thoughtbot.com>
2025-10-17 10:40:08 +0200

    Deep Sky Object: difference between the body and the position
    In the same way using `Astronoby::Jupiter.new` enables to get the
    position of the planet in multiple reference frames for a given instant, it
    is necessary to provide an API as convenient for deep-sky objects.
    
    Previously, it was only possible to instantiate the body and its
    position at the same time, having to provide all the information at
    once. Not only this makes things less convenient to get the position at
    different instants, it also requires the code to know about all the
    attributes constantly. In a next PR we will handle using deep-sky
    objects in services like the `RiseTransitSetCalculator` and the current
    implementation of `DeepSkyObject` makes things hard.
    
    Now, with a difference between the body (`DeepSkyObject`) and the
    position (`DeepSkyObjectPosition`), it is possible to manipulate the
    body accross the code and instantiate it (get a position) whenever
    necessary.

--- lib/astronoby/bodies/deep_sky_object.rb
+++ lib/astronoby/bodies/deep_sky_object.rb
@@ -2,11 +2,6 @@
 
 module Astronoby
   class DeepSkyObject
-    DEFAULT_DISTANCE = Distance.from_parsecs(1e9)
-
-    attr_reader :instant, :apparent
-
-    # @param instant [Astronoby::Instant] Instant of the observation
     # @param equatorial_coordinates [Astronoby::Coordinates::Equatorial]
     #   Equatorial coordinates at epoch J2000.0
     # @param proper_motion_ra [Astronoby::AngularVelocity, nil] Proper motion in
@@ -16,112 +11,34 @@ module Astronoby
     # @param parallax [Astronoby::Angle, nil] Parallax angle
     # @param radial_velocity [Astronoby::Velocity, nil] Radial velocity
     def initialize(
-      instant:,
       equatorial_coordinates:,
-      ephem: nil,
       proper_motion_ra: nil,
       proper_motion_dec: nil,
       parallax: nil,
       radial_velocity: nil
     )
-      @instant = instant
       @initial_equatorial_coordinates = equatorial_coordinates
       @proper_motion_ra = proper_motion_ra
       @proper_motion_dec = proper_motion_dec
       @parallax = parallax
       @radial_velocity = radial_velocity
-      if ephem
-        @earth_geometric = Earth.geometric(ephem: ephem, instant: @instant)
-      end
-      compute_apparent
     end
 
-    # @return [Astronoby::Astrometric] Astrometric position of the object
-    def astrometric
-      @astrometric ||= Astrometric.new(
-        instant: @instant,
-        position: astrometric_position,
-        velocity: astrometric_velocity,
-        center_identifier: SolarSystemBody::EARTH,
-        target_body: self
-      )
-    end
-
-    def observed_by(observer)
-      Topocentric.build_from_apparent(
-        apparent: @apparent,
-        observer: observer,
-        instant: @instant,
-        target_body: self
-      )
-    end
-
-    private
-
-    def astrometric_position
-      @astrometric_position ||= if use_stellar_propagation?
-        stellar_propagation.position
-      else
-        astronomical_distance = DEFAULT_DISTANCE.meters
-        right_ascension = @initial_equatorial_coordinates.right_ascension
-        declination = @initial_equatorial_coordinates.declination
-        Distance.vector_from_meters([
-          declination.cos * right_ascension.cos * astronomical_distance,
-          declination.cos * right_ascension.sin * astronomical_distance,
-          declination.sin * astronomical_distance
-        ])
-      end
-    end
-
-    def astrometric_velocity
-      @astrometric_velocity ||= if use_stellar_propagation?
-        stellar_propagation.velocity_vector
-      else
-        Velocity.vector_from_meters_per_second([0.0, 0.0, 0.0])
-      end
-    end
-
-    def use_stellar_propagation?
-      @proper_motion_ra && @proper_motion_dec && @parallax && @radial_velocity
-    end
-
-    def stellar_propagation
-      @stellar_propagation ||= StellarPropagation.new(
+    # @param instant [Astronoby::Instant] Instant of the observation
+    # @param ephem [Astronoby::Ephemeris, nil] Ephemeris to use for Earth
+    #   position calculation
+    # @return [Astronoby::DeepSkyObjectPosition] Position of the deep-sky object
+    #   at the given instant
+    def at(instant, ephem: nil)
+      DeepSkyObjectPosition.new(
+        instant: instant,
         equatorial_coordinates: @initial_equatorial_coordinates,
+        ephem: ephem,
         proper_motion_ra: @proper_motion_ra,
         proper_motion_dec: @proper_motion_dec,
         parallax: @parallax,
-        radial_velocity: @radial_velocity,
-        instant: @instant,
-        earth_geometric: @earth_geometric
+        radial_velocity: @radial_velocity
       )
-    end
-
-    def compute_apparent
-      @apparent = if @earth_geometric
-        Apparent.build_from_astrometric(
-          instant: @instant,
-          target_astrometric: astrometric,
-          earth_geometric: @earth_geometric,
-          target_body: self
-        )
-      else
-        precession_matrix = Precession.matrix_for(@instant)
-        nutation_matrix = Nutation.matrix_for(@instant)
-        corrected_position = Distance.vector_from_meters(
-          precession_matrix * nutation_matrix * astrometric.position.map(&:m)
-        )
-        corrected_velocity = Velocity.vector_from_mps(
-          precession_matrix * nutation_matrix * astrometric.velocity.map(&:mps)
-        )
-        Apparent.new(
-          position: corrected_position,
-          velocity: corrected_velocity,
-          instant: @instant,
-          center_identifier: SolarSystemBody::EARTH,
-          target_body: self
-        )
-      end
     end
   end
 end
